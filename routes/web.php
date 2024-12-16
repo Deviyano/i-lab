773b1b03cf31ca34d22a28bb717a63d965166378
user <user@local>
2024-12-16 13:50:01 +0100

    Quiz vragen kunnen nu worden opgehaald.

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\quizController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/quiz', [quizController::class, 'quiz_vraag']);
+Route::get('/quiz/{id}', [quizController::class, 'get_question']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
